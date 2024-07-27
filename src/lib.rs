6c79ea331cb55a11af7692fedc34baaa9a6b7e7b
Alisa Sireneva <me@purplesyringa.moe>
2024-07-27 17:20:40 +0300

    Move ExceptionMapper to a separate file

--- src/lib.rs
+++ src/lib.rs
@@ -158,6 +158,8 @@ pub use anyhow_compat::AnyhowContext;
 mod iex_result;
 mod result;
 
+mod exception_mapper;
+
 pub mod example;
 
 struct IexPanic;
@@ -226,51 +228,7 @@ pub mod imp {
 
     impl<E> Copy for Marker<E> {}
 
-    pub struct ExceptionMapper<S, T, U, F: FnOnce(S, T) -> U> {
-        state: ManuallyDrop<S>,
-        f: ManuallyDrop<F>,
-        phantom: PhantomData<fn(S, T) -> U>,
-    }
-
-    impl<S, T, U, F: FnOnce(S, T) -> U> ExceptionMapper<S, T, U, F> {
-        pub fn new(_marker: Marker<U>, state: S, f: F) -> Self {
-            Self {
-                state: ManuallyDrop::new(state),
-                f: ManuallyDrop::new(f),
-                phantom: PhantomData,
-            }
-        }
-
-        pub fn get_in_marker(&self) -> Marker<T> {
-            unsafe { Marker::new() }
-        }
-
-        pub fn get_state(&mut self) -> &mut S {
-            &mut self.state
-        }
-
-        pub fn swallow(mut self) {
-            unsafe { ManuallyDrop::drop(&mut self.state) };
-            unsafe { ManuallyDrop::drop(&mut self.f) };
-            std::mem::forget(self);
-        }
-    }
-
-    impl<S, T, U, F: FnOnce(S, T) -> U> Drop for ExceptionMapper<S, T, U, F> {
-        fn drop(&mut self) {
-            // Resolve TLS just once
-            EXCEPTION.with(|exception| unsafe {
-                let exception = exception.get();
-                // Dereference twice instead of keeping a &mut around, because self.0() may call a
-                // function that uses 'exception'.
-                if let Some(error) = (*exception).read::<T>() {
-                    let state = ManuallyDrop::take(&mut self.state);
-                    let f = ManuallyDrop::take(&mut self.f);
-                    (*exception).write::<U>(f(state, error));
-                }
-            })
-        }
-    }
+    pub use exception_mapper::ExceptionMapper;
 
     pub use iex_result::IexResult;
 
